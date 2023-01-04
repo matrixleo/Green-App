c94b2b7825d7a7834526a88f64c1ba5030b9dfe8
Leon B <leonberisha18@gmail.com>
2023-01-04 16:29:21 +0100

    Second Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { ScreenComponent } from './Screens/screen/screen.component';
+import { HomeScreenComponent } from './Screens/home-screen/home-screen.component';
+import { ProjectsScreenComponent } from './Screens/projects-screen/projects-screen.component';
+import { RoofingScreenComponent } from './Screens/projects-screen/roofing-screen/roofing-screen.component';
+import { RestorationScreenComponent } from './Screens/projects-screen/restoration-screen/restoration-screen.component';
+import { ConstructionScreenComponent } from './Screens/projects-screen/construction-screen/construction-screen.component';
+const routes: Routes = [
+  {path:'', component: ScreenComponent,
+  children : [
+    {path: 'home', component:HomeScreenComponent},
+    {path:'projects', component: ProjectsScreenComponent},
+    {path:'projects/restoration', component:RestorationScreenComponent},
+    {path:'projects/construction', component:ConstructionScreenComponent},
+    {path:'projects/roofing', component:RoofingScreenComponent},
+    
+  ]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,58 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidemenuComponent } from './sideMenu/sidemenu/sidemenu.component';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatTableModule} from '@angular/material/table';
+import { ScreenComponent } from './Screens/screen/screen.component';
+import { HomeScreenComponent } from './Screens/home-screen/home-screen.component';
+import { ProjectsScreenComponent } from './Screens/projects-screen/projects-screen.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from './modules/material/material.module';
+import { RoofingScreenComponent } from './Screens/projects-screen/roofing-screen/roofing-screen.component';
+import { ConstructionScreenComponent } from './Screens/projects-screen/construction-screen/construction-screen.component';
+import { RestorationScreenComponent } from './Screens/projects-screen/restoration-screen/restoration-screen.component';
+import { DetailsScreenComponent } from './Screens/projects-screen/details-screen/details-screen.component';
+
+const Ux_Modules = [
+  MatIconModule,
+  MatSidenavModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatButtonModule,
+  MatListModule,
+  MatExpansionModule,
+  MatTableModule
+]
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidemenuComponent,
+    ScreenComponent,
+    HomeScreenComponent,
+    ProjectsScreenComponent,
+    RoofingScreenComponent,
+    ConstructionScreenComponent,
+    RestorationScreenComponent,
+    DetailsScreenComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    Ux_Modules,
+    HttpClientModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
